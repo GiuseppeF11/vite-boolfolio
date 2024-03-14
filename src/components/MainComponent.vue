e880be755e3ed930ee5c1e4cd8b1edc9a61cd084
GiuseppeF11 <faillagiuseppe13@gmail.com>
2024-03-14 18:15:48 +0100

    Completata Milestone 4/5 + Bonus (paginazione)

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,9 +1,45 @@
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      message: 'MAIN'
+      projects: [],
+      currentPage: 1,
+      lastPage: 1,
     }
+  },
+
+  created() {
+    this.getProjects(this.currentPage);
+  },
+  methods: {
+    getProjects(page) {
+      axios
+          .get('http://127.0.0.1:8000/api/projects', {
+            params: {
+              page: page
+            }
+          })
+          .then(response => {
+              console.log(response.data.results.data);
+              this.projects = response.data.results.data;
+              this.currentPage = response.data.results.current_page;
+              this.lastPage = response.data.results.last_page;
+          })
+    },
+
+    prewPage() {
+      if (this.currentPage > 1) {
+        this.getProjects(this.currentPage - 1);
+      }
+    },
+
+    nextPage() {
+      if (this.currentPage < this.lastPage) {
+        this.getProjects(this.currentPage + 1);
+      }
+    },
   }
 };
 </script>
@@ -11,15 +47,85 @@ export default {
 <template>
 
   <main>
-    <h1>
-      {{ message }}
+    <h1 class="title">
+      Progetti
     </h1>
+    <div class="container">
+      <div class="row row-cols-3">
+        <div v-for="project in projects" :key="project.id" class="col">
+          <div class="card m-3 p-2">
+            <h3>
+              {{ project.id}}) {{ project.title}}
+            </h3>
+            <div v-if="project.cover_img != null">
+                <img :src="'http://127.0.0.1:8000/storage/'+ project.cover_img" :alt="project.title">
+            </div>
+            <p>
+              {{ project.description}}
+            </p>
+            <p>
+              <strong>Categorie: </strong>
+              <div>{{ project.type.title}}</div>
+            </p>
+            <p v-if="project.technologies.length > 0">
+              <strong>
+                Tecnologie:
+              </strong>
+              <ul v-for="technology in project.technologies">
+                  <li>{{ technology.title}}</li>
+              </ul>
+            </p>
+          </div>
+      </div>
+      </div>
+    </div>
+    <div class="pages-nav row w-50 m-auto justify-content-center text-center ">
+      <button class="col-auto btn btn-success mx-2" @click="prewPage()">Prew</button>
+      <button class="col-auto btn btn-success mx-2" @click="nextPage()">Next</button>
+        <div>Pagina {{ currentPage }} di {{ lastPage }}</div>
+    </div>
   </main>
 
 </template>
 
 <style scoped>
-h1 {
-  background-color: rgb(57, 169, 32);
+main {
+  background-color: rgb(94, 118, 89);
+  padding: 20px 0;
+}
+
+.title {
+  background-color: rgb(157, 255, 0);
+  box-shadow: 0px 0px 10px rgb(0, 0, 0);
+  text-align: center;
+  margin: 0 auto;
+  width: 30vw;
+  padding: 5px 20px;
+  border-radius: 20px;
+  cursor: pointer;
+  &:hover {
+    animation: bounce 0.5s infinite alternate;
+  }
+}
+@keyframes bounce {
+  to { transform: scale(1.03); }
+}
+
+.card {
+  background-color: rgb(101, 184, 0);
+  box-shadow: 5px 5px 10px black;
+  border-radius: 10px;
+  margin: 10px;
+  cursor: pointer;
+  transition: ease 0.5s;
+  
+    &:hover {
+      background-color: rgb(183, 255, 0);
+      scale: 1.03;
+    }
+}
+
+.pages-nav {
+
 }
 </style>
